476b12049784816a436c50aed0d9ca72f36bacac
DrozdetskySergey <master@2867073.ru>
2021-07-13 00:36:37 +0700

    Решение Range часть 1

--- Range/src/ru/academitschool/drozdetsky21/Range.java
+++ Range/src/ru/academitschool/drozdetsky21/Range.java
@@ -25,5 +25,31 @@ public class Range {
         this.to = to;
     }
 
+    public double getLength() {
+        return to - from;
+    }
 
+    public boolean isInside(double number) {
+        return from <= number && number <= to;
+    }
+
+    private static void displayMessageAboutRangeWithNumber(Range range, double number) {
+        double rangeStart = range.getFrom();
+        double rangeEnd = range.getTo();
+        double rangeLength = range.getLength();
+        String message = range.isInside(number) ? "входит в диапазон" : "за пределами диапазона";
+
+        System.out.printf("Диапазон от %.4f до %.4f, его длина %.4f, число %.4f %s%n", rangeStart, rangeEnd, rangeLength, number, message);
+    }
+
+    public static void main(String[] args) {
+        Range range = new Range(1, 5.7);
+
+        displayMessageAboutRangeWithNumber(range, 0);
+
+        range.setFrom(-3.3333);
+        range.setTo(21.3333);
+
+        displayMessageAboutRangeWithNumber(range, 1);
+    }
 }
