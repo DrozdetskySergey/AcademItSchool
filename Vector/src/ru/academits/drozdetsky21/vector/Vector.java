828268d46219b4c45f52676849ee7127abfd0832
DrozdetskySergey <master@2867073.ru>
2021-07-21 17:32:34 +0700

    Задача Vector. Части 1, 2, 3.

--- Vector/src/ru/academits/drozdetsky21/vector/Vector.java
+++ Vector/src/ru/academits/drozdetsky21/vector/Vector.java
@@ -1,6 +1,61 @@
 package ru.academits.drozdetsky21.vector;
 
+import java.util.Arrays;
+
 public class Vector {
     private double[] array;
 
+    public Vector(int n) {
+        if (n <= 0) {
+            throw new IllegalArgumentException("n < 1");
+        }
+
+        array = new double[n];
+        Arrays.fill(array, 0);
+    }
+
+    public Vector(Vector vector) {
+        if (vector == null) {
+            throw new IllegalArgumentException("NULL");
+        }
+
+        array = vector.toArray();
+    }
+
+    public Vector(double[] array) {
+        if (array == null || array.length == 0) {
+            throw new IllegalArgumentException("NULL | n < 1");
+        }
+
+        this.array = Arrays.copyOf(array, array.length);
+    }
+
+    public Vector(int n, double[] array) {
+        if (array == null || n <= 0) {
+            throw new IllegalArgumentException("NULL | n < 1");
+        }
+
+        this.array = Arrays.copyOf(array, n);
+    }
+
+    public int getSize() {
+        return array.length;
+    }
+
+    public double[] toArray() {
+        return Arrays.copyOf(array, array.length);
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder stringBuilder = new StringBuilder("{");
+
+        for (double element : array) {
+            stringBuilder.append(element).append(", ");
+        }
+
+        stringBuilder.delete(stringBuilder.length() - 2, stringBuilder.length()).append("}");
+
+        return stringBuilder.toString();
+    }
 }
--- Vector/src/ru/academits/drozdetsky21/vector_test/Main.java
+++ Vector/src/ru/academits/drozdetsky21/vector_test/Main.java
@@ -1,4 +1,10 @@
 package ru.academits.drozdetsky21.vector_test;
 
+import ru.academits.drozdetsky21.vector.Vector;
+
 public class Main {
+    public static void main(String[] args) {
+        Vector vector = new Vector(new double[]{1.23, 2, 3.333333, 4, 5});
+        System.out.println(vector);
+    }
 }
