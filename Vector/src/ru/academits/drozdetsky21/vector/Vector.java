58dbab95389bfbef1fe1d57577981b1231dd43a8
DrozdetskySergey <master@2867073.ru>
2021-07-21 23:54:33 +0700

    Задача Vector. Части 4.

--- Vector/src/ru/academits/drozdetsky21/vector/Vector.java
+++ Vector/src/ru/academits/drozdetsky21/vector/Vector.java
@@ -7,7 +7,7 @@ public class Vector {
 
     public Vector(int n) {
         if (n <= 0) {
-            throw new IllegalArgumentException("n < 1");
+            throw new IllegalArgumentException("length < 1");
         }
 
         array = new double[n];
@@ -24,7 +24,7 @@ public class Vector {
 
     public Vector(double[] array) {
         if (array == null || array.length == 0) {
-            throw new IllegalArgumentException("NULL | n < 1");
+            throw new IllegalArgumentException("NULL | length < 1");
         }
 
         this.array = Arrays.copyOf(array, array.length);
@@ -32,14 +32,10 @@ public class Vector {
 
     public Vector(int n, double[] array) {
         if (array == null || n <= 0) {
-            throw new IllegalArgumentException("NULL | n < 1");
+            throw new IllegalArgumentException("NULL | length < 1");
         }
 
         this.array = Arrays.copyOf(array, n);
-    }
-
-    public int getSize() {
-        return array.length;
     }
 
     public double[] toArray() {
@@ -57,5 +53,76 @@ public class Vector {
         stringBuilder.delete(stringBuilder.length() - 2, stringBuilder.length()).append("}");
 
         return stringBuilder.toString();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
+        Vector vector = (Vector) o;
+
+        return Arrays.equals(array, vector.array);
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+
+        for (double element : array) {
+            result = prime * result + (int) element;
+        }
+
+        return result;
+    }
+
+    public Vector doAddition(Vector vector) {
+        int length = Math.min(this.array.length, vector.array.length);
+
+        for (int i = 0; i < length; i++) {
+            array[i] += vector.array[i];
+        }
+
+        return this;
+    }
+
+    public Vector doSubtraction(Vector vector) {
+        int length = Math.min(this.array.length, vector.array.length);
+
+        for (int i = 0; i < length; i++) {
+            array[i] -= vector.array[i];
+        }
+
+        return this;
+    }
+
+    public Vector doMultiplication(double number) {
+        for (int i = 0; i < array.length; i++) {
+            array[i] *= number;
+        }
+
+        return this;
+    }
+
+    public Vector doReverse() {
+        return doMultiplication(-1);
+    }
+
+    public int getSize() {
+        return array.length;
+    }
+
+    public double get(int index) {
+        return array[index];
+    }
+
+    public void set(int index, double element) {
+        array[index] = element;
     }
 }
--- Vector/src/ru/academits/drozdetsky21/vector_test/Main.java
+++ Vector/src/ru/academits/drozdetsky21/vector_test/Main.java
@@ -4,7 +4,13 @@ import ru.academits.drozdetsky21.vector.Vector;
 
 public class Main {
     public static void main(String[] args) {
-        Vector vector = new Vector(new double[]{1.23, 2, 3.333333, 4, 5});
-        System.out.println(vector);
+        Vector vector1 = new Vector(new double[]{1.23, 2, 3.333333, 4, 5});
+        Vector vector2 = new Vector(vector1);
+        Vector vector3 = new Vector(3, new double[]{10, 10, 10, 10, 10, 10, 10});
+
+        vector1.doReverse().doAddition(vector3).doSubtraction(vector2).set(1, vector1.get(4));
+        System.out.println(vector1);
+
+        Vector vector4 = new Vector(-3);
     }
 }
