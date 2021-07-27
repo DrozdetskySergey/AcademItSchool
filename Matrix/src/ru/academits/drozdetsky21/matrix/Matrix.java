80222b1a1459140911147153a15b3ea162efb340
DrozdetskySergey <master@2867073.ru>
2021-07-28 01:19:41 +0700

    Задача Matrix. Часть 2 до умножения, без определителя.

--- Matrix/src/ru/academits/drozdetsky21/matrix/Matrix.java
+++ Matrix/src/ru/academits/drozdetsky21/matrix/Matrix.java
@@ -7,25 +7,25 @@ import java.util.Arrays;
 public class Matrix {
     private Vector[] rows;
 
-    public Matrix(int columnsCount, int rowsCount) {
-        if (columnsCount <= 0) {
-            throw new IllegalArgumentException("columnsCount = " + columnsCount + "; У матрицы должен быть минимум 1 столбец.");
+    public Matrix(int rows, int columns) {
+        if (columns <= 0) {
+            throw new IllegalArgumentException("Конструктор Matrix: columns = " + columns + "; У матрицы должен быть минимум 1 столбец.");
         }
 
-        if (rowsCount <= 0) {
-            throw new IllegalArgumentException("rowsCount = " + rowsCount + "; У матрицы должна быть минимум 1 строка.");
+        if (rows <= 0) {
+            throw new IllegalArgumentException("Конструктор Matrix: rows = " + rows + "; У матрицы должна быть минимум 1 строка.");
         }
 
-        rows = new Vector[rowsCount];
+        this.rows = new Vector[rows];
 
-        for (int i = 0; i < rowsCount; i++) {
-            rows[i] = new Vector(columnsCount);
+        for (int i = 0; i < rows; i++) {
+            this.rows[i] = new Vector(columns);
         }
     }
 
     public Matrix(Matrix matrix) {
         if (matrix == null) {
-            throw new IllegalArgumentException("Переданная матрицы ссылается на NULL.");
+            throw new IllegalArgumentException("Конструктор Matrix(Matrix): Переданная матрицы ссылается на NULL.");
         }
 
         rows = new Vector[matrix.rows.length];
@@ -37,11 +37,11 @@ public class Matrix {
 
     public Matrix(double[][] array) {
         if (array == null) {
-            throw new IllegalArgumentException("Переданный массив double[][] ссылается на NULL.");
+            throw new IllegalArgumentException("Конструктор Matrix(double[][]): Переданный массив ссылается на NULL.");
         }
 
         if (array.length == 0) {
-            throw new IllegalArgumentException("array.length == 0; У матрицы должна быть минимум 1 строка.");
+            throw new IllegalArgumentException("Конструктор Matrix(double[][]): Длинна массива = 0; У матрицы должна быть минимум 1 строка.");
         }
 
         int rowMaxLength = 0;
@@ -53,7 +53,7 @@ public class Matrix {
         }
 
         if (rowMaxLength == 0) {
-            throw new IllegalArgumentException("У матрицы должен быть минимум 1 столбец.");
+            throw new IllegalArgumentException("Конструктор Matrix(double[][]): Длина всех вложенных массивов = 0; У матрицы должен быть минимум 1 столбец.");
         }
 
         rows = new Vector[array.length];
@@ -65,11 +65,11 @@ public class Matrix {
 
     public Matrix(Vector[] vectors) {
         if (vectors == null) {
-            throw new IllegalArgumentException("Переданный массив Vector[] ссылается на NULL.");
+            throw new IllegalArgumentException("Конструктор Matrix(Vector[]): Переданный массив ссылается на NULL.");
         }
 
         if (vectors.length == 0) {
-            throw new IllegalArgumentException("vectors.length == 0; У матрицы должна быть минимум 1 строка.");
+            throw new IllegalArgumentException("Конструктор Matrix(Vector[]): Длинна массива = 0; У матрицы должна быть минимум 1 строка и 1 столбец.");
         }
 
         int vectorMaxSize = 0;
@@ -82,15 +82,115 @@ public class Matrix {
             }
         }
 
-        if (vectorMaxSize == 0) {
-            throw new IllegalArgumentException("У матрицы должен быть минимум 1 столбец.");
-        }
-
         rows = new Vector[vectors.length];
 
         for (int i = 0; i < vectors.length; i++) {
             this.rows[i] = new Vector(vectorMaxSize, vectors[i].toArray());
         }
+    }
+
+    public int getRowsCount() {
+        return rows.length;
+    }
+
+    public int getColumnsCount() {
+        return rows[0].getSize();
+    }
+
+    public Vector getRow(int index) {
+        return rows[index];
+    }
+
+    public void setRow(int index, Vector row) {
+        if (row == null) {
+            throw new IllegalArgumentException("Замена ветора-строки в матрице по индексу: Переданный вектор ссылается на NULL.");
+        }
+
+        if (row.getSize() != rows[0].getSize()) {
+            throw new IllegalArgumentException("Замена ветора-строки в матрице по индексу: Переданный вектор не подходящей длины.");
+        }
+
+        rows[index] = row;
+    }
+
+    public Vector getColumn(int index) {
+        double[] array = new double[rows.length];
+
+        for (int i = 0; i < rows.length; i++) {
+            array[i] = rows[i].get(index);
+        }
+
+        return new Vector(array);
+    }
+
+    public double[][] toArray() {
+        double[][] result = new double[rows.length][];
+
+        for (int i = 0; i < result.length; i++) {
+            result[i] = rows[i].toArray();
+        }
+
+        return result;
+    }
+
+    public Matrix transpose() {
+        double[][] array = toArray();
+        double[][] result = new double[array[0].length][array.length];
+
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[0].length; j++) {
+                result[j][i] = array[i][j];
+            }
+        }
+
+        rows = new Vector[result.length];
+
+        for (int i = 0; i < rows.length; i++) {
+            rows[i] = new Vector(result[i]);
+        }
+
+        return this;
+    }
+
+    public Matrix multiply(double number) {
+        for (Vector row : rows) {
+            row.multiply(number);
+        }
+
+        return this;
+    }
+
+    public Matrix multiply(Matrix matrix) {
+        if (rows[0].getSize() != matrix.rows.length) {
+            throw new IllegalArgumentException("Умножение матрицы на матрицу: не подходящие размерности (не согласованны).");
+        }
+
+        int matrixColumnsCount = matrix.rows[0].getSize();
+        double[][] result = new double[rows.length][matrixColumnsCount];
+
+        for (int i = 0; i < rows.length; i++) {
+            for (int j = 0; j < matrixColumnsCount; j++) {
+                result[i][j] = Vector.getScalarProduct(rows[i], matrix.getColumn(j));
+            }
+        }
+
+        rows = new Vector[rows.length];
+
+        for (int i = 0; i < rows.length; i++) {
+            rows[i] = new Vector(result[i]);
+        }
+
+        return this;
+    }
+
+    public Matrix multiply(Vector vector) {
+        if (rows[0].getSize() != vector.getSize()) {
+            throw new IllegalArgumentException("Умножение матрицы на вектор: не подходящие размерности.");
+        }
+
+        Matrix matrix = new Matrix(new Vector[]{vector});
+
+        return multiply(matrix.transpose());
     }
 
     @Override
--- Matrix/src/ru/academits/drozdetsky21/matrix_test/Main.java
+++ Matrix/src/ru/academits/drozdetsky21/matrix_test/Main.java
@@ -1,15 +1,41 @@
 package ru.academits.drozdetsky21.matrix_test;
 
 import ru.academits.drozdetsky21.matrix.Matrix;
+import ru.academits.drozdetsky21.vector.Vector;
+
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        Matrix matrix = new Matrix(new double[][]{
+        Matrix matrix1 = new Matrix(new double[][]{
                 {1, 2, 3, 3},
                 {4, 5, 6},
                 {7, 8, 9, 10, 11, 12}
         });
 
-        System.out.println(matrix);
+        System.out.println(matrix1);
+        System.out.println(Arrays.deepToString(matrix1.toArray()));
+        System.out.println(matrix1.getColumnsCount() + ", " + matrix1.getRowsCount());
+
+        matrix1.multiply(0.5).transpose();
+        System.out.println(Arrays.deepToString(matrix1.toArray()));
+
+        Matrix matrix2 = new Matrix(new double[][]{
+                {1, 2},
+                {3, 4},
+                {5, 6},
+                {7, 8}
+        });
+
+        Matrix matrix3 = new Matrix(new double[][]{
+                {1, 2, 3},
+                {4, 5, 6}
+        });
+
+        System.out.println(matrix2.multiply(matrix3));
+
+        Vector vector1 = new Vector(new double[]{1, 2, 3});
+        System.out.print(matrix1);
+        System.out.println(" * " + vector1 + " = " + matrix1.multiply(vector1));
     }
 }
